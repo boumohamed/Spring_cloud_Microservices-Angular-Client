b1c70056bff3cc8969dfab247b217ed73b131754
boumohamed <mb.bouzri@gmail.com>
2022-11-15 21:24:19 +0100

    refactoring : Order Implimentation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http'
+import { CategoriesModule } from './categories/categories.module';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ProductsModule } from './products/products.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ProductsModule,
+    CategoriesModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
